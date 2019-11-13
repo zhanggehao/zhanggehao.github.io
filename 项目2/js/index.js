0b7e7b1a335ce2331923d59f6bc1fad771d2caea
zhanggehao <57182359+zhanggehao@users.noreply.github.com>
2019-11-13 23:48:38 +0800

    Add files via upload

---
+++ 项目2/js/index.js
@@ -0,0 +1,221 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title>李文个人站点</title>
+<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
+<link rel="stylesheet" type="text/css" href="css/base.css"/>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+<div class="top">
+<div class="nav">
+<div class="nav_tab">
+	       <span class="nav_button1"></span>
+		   <a href="./index.html" class="tab1">简介</a>
+		   <a href="./product.html" class="tab2">作品</a>
+		   <a href="./detail.html" class="tab3">产品</a>
+        </div>
+<div class="nav_function">		
+		   <img src="images/search_icon.png">
+		   <img src="images/function_icon.png">
+		</div>
+		
+    </div>
+<div class="top_title">
+		   <span>李文个人Page</span>
+		   <p>A fool thinks he's smart，A wise man knows he's a fool </p>
+	   </div>
+<div class="top_bj"><img class="top_img" src="images/1page_bj.png"></div>
+</div>
+
+<div class="introduce">
+	
+		   <div class="introduce_title"><img src="images/title_icon.png"></div>
+		   <div class="introduce_content1"><sapn >想要得到尊重，首先要成为一个可以被尊重的人。作为设计师，核心竞争力是：设计能力、设计管理能力、严格的执行力、良好的内涵、可以信赖的外表、综合知识水平、快速反应能力，设计师需要建立自信，而自信正是来自于自身的实力。</sapn>
+			<ul>我的设计价值是：
+					<li>1、能够有效的解决问题。</li>
+					<li>2、设计资源的合情理利用。</li>
+					<li>3、有服务大众的同理心，创造搞笑、美观、环保、底价的产品。</li>
+					<li>4、对用户的思考。</li>
+				</ul>
+				
+		</div>
+		<div class="line_01"></div>
+</div>
+
+<div class="product">
+
+	<div class="product_tab">
+		<a class="bta" href="javascript:;" idx="1">Web<span class="btspan"></span></a>
+		<a href="javascript:;" idx="2">APP<span class=""></span></a>
+		<a href="javascript:;" idx="3">H5<span class=""></span></a>
+		<a href="javascript:;" idx="4">平面<span class=""></span></a>
+		<a href="javascript:;" idx="5">动效<span class=""></span></a>
+</div>
+
+
+  <section id="content1" class="product_box" style="display: block;">
+    <div class="case1">
+		<img src="images/case1_bj.png">
+		<title>云财网</title>
+		<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+		</p>
+		<a href="./detail.html" class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+	</div>
+	<div class="case1">
+			<img src="images/case1_bj.png">
+			<title>云财网</title>
+			<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+			</p>
+			<a  href="./detail.html" class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+		</div>
+	<div class="case2">
+				<img src="images/case1_bj.png">
+				<title>云财网</title>
+				<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+				</p>
+				<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+			</div>
+  </section>
+
+
+  <section id="content2" class="product_box">
+    <div class="case1">
+		<img src="images/case1_bj.png">
+		<title>云财网</title>
+		<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+		</p>
+		<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+	</div>
+	<div class="case1">
+			<img src="images/case1_bj.png">
+			<title>云财网</title>
+			<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+			</p>
+			<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+		</div>
+	<div class="case2">
+				<img src="images/case1_bj.png">
+				<title>云财网</title>
+				<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+				</p>
+				<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+			</div>
+  </section>
+
+
+  <section id="content3" class="product_box">
+    <div class="case1">
+		<img src="images/case1_bj.png">
+		<title>云财网</title>
+		<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+		</p>
+		<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+	</div>
+	<div class="case1">
+			<img src="images/case1_bj.png">
+			<title>云财网</title>
+			<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+			</p>
+			<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+		</div>
+	<div class="case2">
+				<img src="images/case1_bj.png">
+				<title>云财网</title>
+				<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+				</p>
+				<a  href="./detail.html"  class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+			</div>
+  </section>
+
+
+  <section id="content4" class="product_box">
+    <div class="case1">
+		<img src="images/case1_bj.png">
+		<title>云财网</title>
+		<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+		</p>
+		<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+	</div>
+	<div class="case1">
+			<img src="images/case1_bj.png">
+			<title>云财网</title>
+			<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+			</p>
+			<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+		</div>
+	<div class="case2">
+				<img src="images/case1_bj.png">
+				<title>云财网</title>
+				<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+				</p>
+				<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+			</div>
+  </section>
+
+
+  <section id="content5" class="product_box">
+    <div class="case1">
+		<img src="images/case1_bj.png">
+		<title>云财网</title>
+		<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+		</p>
+		<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+	</div>
+	<div class="case1">
+			<img src="images/case1_bj.png">
+			<title>云财网</title>
+			<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+			</p>
+			<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+		</div>
+	<div class="case2">
+				<img src="images/case1_bj.png">
+				<title>云财网</title>
+				<p>“云财网”服务范围包含个人信用评估、小额信用借款、金融信息服务为一体，致力于成为中国金融信息服务领域里最具专业性、最值得信赖的微金融服务公司。
+				</p>
+				<a class="case1_button" id="case1_button" style="vertical-align: inherit;">查看详情</a>
+			</div>
+  </section>
+</div>
+
+<div class="footer">
+	<img src="images/footer_bj.png">
+	<div class="record">粤ICP备1512455537号-2</div>
+
+</div>
+
+<script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+<script>
+	$(function() {
+		$('.product_tab a').click(function() {
+			$('.product_box').hide();
+			$('.product_tab span').removeClass('btspan');
+			$('.product_tab a').removeClass('bta');
+
+			$('#content' + $(this).attr('idx')).show();
+			$(this).find('span').addClass('btspan');
+			$(this).addClass('bta');
+		})
+	})
+
+</script>
+<script>
+		$(document).ready(function(){
+		  $("#case1_button").mouseover(function(){
+			$("#case1_button").css("background-color","#303030");
+			$("#case1_button").css("color","#fff");
+		  });
+		  $("#case1_button").mouseout(function(){
+			$("#case1_button").css("background-color","");
+			$("#case1_button").css("color","#303030");
+		  });
+		});
+		</script>
+</body>
+</html>
